6be07ab606eb31d96e195f75eacad416edf98239
mmorillo <matheus.morillo@nescara.com>
2019-07-26 17:31:26 -0300

    improved

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opt, qtdVertices, fst, scd;
+        boolean clearBuffer = false;
         Dijkstra dijkstra;
 
         System.out.println("Number of vertices: ");
-        qtdVertices = scanner.nextInt();
+        qtdVertices = getInt(scanner);
+
+        if(qtdVertices < 0) {
+            System.err.println("Invalid number!!");
+            return;
+        }
 
         dijkstra = new Dijkstra(qtdVertices, true);
 
         do {
-            System.out.println(getPanel());
-            opt = scanner.nextInt();
+            System.out.print(getPanel());
+
+            if(clearBuffer)
+                scanner.nextLine();
+
+            opt = getInt(scanner);
+
+            if(opt == -1)
+                clearBuffer = true;
+            else
+                clearBuffer = false;
 
             if(opt == 1) {
                 System.out.println("Type two vertices to be linked: ");
 
-                fst = scanner.nextInt();
-                scd = scanner.nextInt();
+                fst = getInt(scanner);
+                scd = getInt(scanner);
 
                 if(!dijkstra.link(fst, scd)) {
                     System.out.println("Failed Linking!");
@@ -28,8 +44,8 @@ public class Main {
             else if(opt == 2) {
                 System.out.println("Type two vertices to be unlinked: ");
 
-                fst = scanner.nextInt();
-                scd = scanner.nextInt();
+                fst = getInt(scanner);
+                scd = getInt(scanner);
 
                 if (!dijkstra.unLink(fst, scd)) {
                     System.out.println("Failed to unlink");
@@ -38,18 +54,37 @@ public class Main {
             else if(opt == 3) {
                 System.out.println(dijkstra.toString());
             }
+            else if(opt == 4) {
+                cls();
+            }
             else if(opt == 0);
             else {
-                System.out.println("Invalid Option");
+                System.err.println("Invalid Option!\n");
             }
         } while(opt != 0);
+    }
+
+    //This method is used to encapsulate try catch block with nextInt method
+    private static int getInt(Scanner scanner) {
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            return -1;
+        }
     }
 
     private static String getPanel() {
         return  "1- Link\n" +
                 "2- Unlink\n" +
                 "3- Show connections\n" +
-                "0- Exit\n" +
+                "4- Clear Log\n" +
+                "0- Exit\n\n" +
                 "Option: ";
+    }
+
+    private static void cls() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
     }
 }
